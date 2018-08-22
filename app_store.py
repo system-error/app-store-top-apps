57f51b88896f0bc2da6c92c99000c138394d018a
Christos Dousmanopoulos <system-error@users.noreply.github.com>
2018-08-22 12:19:43 +0300

    update
    put some lines to export as json to see how it looks like

--- app_store.py
+++ app_store.py
@@ -1,26 +1,42 @@
-from collections import Counter
-import csv,time
+from collections import Counter,defaultdict
+import csv,time,json 
 start = time.time()
 top_apps = set()
 prime_genre_list = []
 user_rating_list = []
+track_name_list = []
 applications = {}
 apps_list = []
-appis = {}
+appis = defaultdict(list)
 with open('AppleStore1.csv',mode='r',encoding="utf8") as csvfile:
     app_store = csv.DictReader(csvfile)
     for row in app_store:
-        prime_genre,user_rating = row["prime_genre"],row["user_rating"]
-        prime_genre_list.append(prime_genre)
-        user_rating_list.append(user_rating)
-    for apps in zip(prime_genre_list,user_rating_list):
+        prime_genre,user_rating,track_name = row["prime_genre"],row["user_rating"],row["track_name"]
+        if user_rating > '3.5':
+            prime_genre_list.append(prime_genre)
+            user_rating_list.append(user_rating)
+            track_name_list.append(track_name)
+        # prime_genre_list.append(prime_genre)
+        # user_rating_list.append(user_rating)    
+
+    for apps in zip(prime_genre_list,user_rating_list,track_name_list):
         apps_list.append(apps)
     print()  
-    for i,z in apps_list:
-        appis[i] = z
+    for i,z,y in apps_list:
+        appis[i].append(z)
+        appis[i].append(y)
     print(appis)
+    # for i,z,y in apps_list:
+    #     applications[i] = z,y
+    #     # applications[i] = y
+    # print(applications)
+js = json.dumps(appis, sort_keys=False, indent=4, separators=(',', ': '))    
+with open('result.json', 'w') as fp:
 
-    # print(apps_list)    
+    fp.write(js)
+    fp.close()
+    
+    print(apps_list)    
     # print(sorted(top_apps, key=lambda x: x[1],reverse=True))
         
 
